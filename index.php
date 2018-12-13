34d308acd53c6f54a08242af8ad7f1cc517d8de7
Maria da Graça <maria.graca@empresa.local>
2018-12-13 17:04:26 -0200

    endpoint ok, novos estilos e bibliotecas

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 /**
  * @TODO animações ( X )
  * @TODO criar tabela ( V )
- * @TODO criar form ( X )
- * @TODO mandar dados, AJAX ( X )
+ *   - Adicionar data na tabela, no model e dao, e inserir no endpoint?
+ * @TODO criar form ( V )
+ * @TODO mandar dados, AJAX ( V )
  * @TODO conexão segura, reportando erros, try catche e bind ativo ( X )
  * @TODO Layout com bootstrap grid ( X )
+ * @TODO validar dados no back e no front
  */
 ?>
 
@@ -17,59 +19,90 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Formulário de Contratação</title>
     <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="32x32">
-    <link rel="stylesheet" href="/assets/css/normalize.css">
-    <link rel="stylesheet" href="/assets/css/index.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/index.css">
     <!--[if lt IE 9]
         <script src="/assets/js/html5shiv.js" type="text/javascript"></script>
     [endif] -->
 </head>
 <body>
-    <section class="section bgGray">
-        <div class="container"><?php require './assets/html/lista.html'; ?></div>
+    <section class="section bgLightGray">
+        <div class="container listas"><?php require './assets/html/lista.html'; ?></div>
     </section>
-    <section class="section">
+    <section class="section text-center">
         <div class="container">
-            <h1>Se liga nessa novidade</h1>
+            <h1 class="title tx-azul">Se liga nessa novidade</h1>
             <h2>O que é?</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempor lorem id congue luctus. Praesent vel purus finibus, rhoncus mauris et, aliquam dolor.</p>           
-            <p>Praesent nisi mi, condimentum ut viverra at, consectetur non ipsum. Nulla facilisi. In tempor eleifend imperdiet. Proin mollis condimentum enim, a dictum felis pulvinar quis.</p>
-            <section>
-                <h2>Lorem Ipsum?</h2>
-                <figure>
+            <div class="texto-princial">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempor lorem id congue luctus. Praesent vel purus finibus, rhoncus mauris et, aliquam dolor.</p>           
+                <p>Praesent nisi mi, condimentum ut viverra at, consectetur non ipsum. Nulla facilisi. In tempor eleifend imperdiet. Proin mollis condimentum enim, a dictum felis pulvinar quis.</p>
+            </div>
+            <section class="row">
+                <h2 class="col-lg-12">Lorem Ipsum?</h2>
+                <figure class="col-xs-12 col-sm-4">
                     <img src="/assets/img/1.png" alt="1.png">
                     <figcaption>Lorem ipsum dolor sit amet, consectetur adipiscing elit</figcaption>
                 </figure>
-                <figure>
+                <figure class="col-xs-12 col-sm-4">
                     <img src="/assets/img/2.png" alt="2.png">
                     <figcaption>Lorem ipsum dolor sit amet, consectetur adipiscing elit</figcaption>
                 </figure>
-                <figure>
+                <figure class="col-xs-12 col-sm-4">
                     <img src="/assets/img/3.png" alt="3.png">
                     <figcaption>Lorem ipsum dolor sit amet, consectetur adipiscing elit</figcaption>
                 </figure>
-                <figure>
+                <figure class="col-xs-12 col-sm-4">
                     <img src="/assets/img/4.png" alt="4.png">
                     <figcaption>Lorem ipsum dolor sit amet, consectetur adipiscing elit</figcaption>
                 </figure>
-                <figure>
+                <figure class="col-xs-12 col-sm-4">
                     <img src="/assets/img/5.png" alt="5.png">
                     <figcaption>Lorem ipsum dolor sit amet, consectetur adipiscing elit</figcaption>
                 </figure>
-                <figure>
+                <figure class="col-xs-12 col-sm-4">
                     <img src="/assets/img/6.png" alt="6.png">
                     <figcaption>Lorem ipsum dolor sit amet, consectetur adipiscing elit</figcaption>
                 </figure>
             </section>
             <h2>Contrate agora</h2>
-            <form class="caForm" action="">
-                <input type="text">
-                <input type="text">
-                <input type="text">
-                <input type="text">
-                <input type="submit">
+            <form id="caForm" class="caForm" method="post" action="/put/contato/">
+                <legend>Faça um lorem ipsum</legend>
+                <input class="form-control"type="text" name="nome" placeholder="Nome" required>
+                <input class="form-control"type="text" name="email" placeholder="E-mail" required>
+                <input class="form-control"type="text" name="numero" placeholder="Número" required>
+                <input class="form-control"type="text" name="endereco" placeholder="Endereço" required>
+                <input class="btnSubmit"type="submit" name="submit" value="Lorem ipsum agora">
             </form>
         </div>
     </section>
+    <script>
+        function submitCA(event) {
+            event.preventDefault();
+            var endpoint = caForm.getAttribute("action");
+            var data = {
+                nome : caForm.nome.value,
+                email : caForm.email.value,
+                numero : caForm.numero.value,
+                endereco : caForm.endereco.value
+            };
+            $.post(endpoint, data, function (resp) {
+                // @TODO usar a resposta para parar a animação
+                // de rotação
+                document.write(resp);
+            });
+        }
+        
+        caForm.submit.addEventListener("click", submitCA);
+        window.addEventListener("load", function () {
+            var options = {
+                placeholder: "Número (DD) 00000-0000"
+            };            
+            $(caForm.numero).mask("(99) 99999-9999", options);
+        });
+    </script>
+    <script src="/assets/js/jquery/jquery-2.1.1.min.js"></script>
+    <script src="/assets/js/jquery/jquery.mask.min.js"></script>
+    <script src="/assets/js/jquery/jquery.validate.min.js"></script>
 </body>
 </html>
--- put/contato/index.php
+++ put/contato/index.php
@@ -1,11 +1,38 @@
-<!-- @TODO implementar endpoint -->
 <?php
+    /**
+     * Endpoint da tabela contato
+     * Primeiro checa se todos os parametros estão presentes
+     */
+    if  (!isset($_POST['nome'], $_POST['email'], $_POST['numero'], $_POST['endereco'])) {
+        # @TODO ver se consigo implementar code 422 com o ajax
+        http_response_code(200);
+        echo htmlentities('Parâmetro não setado');
+        exit;
+    }
+
     require '../../_backend/DBConnection.Class.php';
     require '../../_backend/dao/ContatoDAO.Class.php';
     require '../../_backend/model/Contato.Class.php';
 
-    #$contato = new Contato('TesteMaual', 'teste@teste.com', 3, 'Casa');
-    #$contatoDAO = new ContatoDAO();
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $numero = $_POST['numero'];
+    $endereco = $_POST['endereco'];
 
-    #$contatoDAO->save($contato);
+
+    try {
+        $contato = new Contato($nome, $email, $numero, $endereco);
+        $contatoDAO = new ContatoDAO();
+    
+        $contatoDAO->save($contato);
+        echo 'Dados Inseridos';
+        // @TODO apagar esse visualizador
+        foreach($_POST as $incoming) {
+            echo htmlentities($incoming), '</br>';
+        }
+    } catch (Exception $ex) {
+        error_log($e->getMessage());
+        http_response_code(500);
+        echo 'Internal Server Error';
+    }
 ?>
