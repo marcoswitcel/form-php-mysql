e894c428f7a64a193252ae6f8691820f71dc3371
Maria da Graça <maria.graca@empresa.local>
2018-12-14 00:26:10 -0200

    endpoint com validações

--- put/contato/index.php
+++ put/contato/index.php
@@ -5,8 +5,33 @@
      */
     if  (!isset($_POST['nome'], $_POST['email'], $_POST['numero'], $_POST['endereco'])) {
         # @TODO ver se consigo implementar code 422 com o ajax
-        http_response_code(200);
-        echo htmlentities('Parâmetro não setado');
+        http_response_code(422);
+        echo htmlentities('Parâmetro(s) não setado(s)');
+        exit;
+    }
+
+    /* Paramêtros */
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $numero = $_POST['numero'];
+    $endereco = $_POST['endereco'];
+    /**
+     * tratando e testando parâmetros 
+     * os que falharem recebem null
+     */
+    $nome = trim($nome);
+    $nome = strlen($nome) > 3 ? $nome : null;
+    $email = trim($email);
+    $email = filter_var($email, FILTER_VALIDATE_EMAIL) ? $email: null;
+    $numero = str_replace(['(', ')', ' ', '-'], '', $numero);
+    $numero = preg_match('/^[0-9]+$/', $numero) ? $numero : '';
+    $numero = strlen($numero) >= 8 ? $numero : null;
+    $endereco = strlen($endereco) > 3 ? $endereco : null;;
+
+    /* Checa se todos os parâmetros passaram no teste */
+    if  (!isset($nome, $email, $numero, $endereco)) {
+        http_response_code(422);
+        echo htmlentities('Parâmetro(s) inválido(s)');
         exit;
     }
 
@@ -14,22 +39,12 @@
     require '../../_backend/dao/ContatoDAO.Class.php';
     require '../../_backend/model/Contato.Class.php';
 
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $numero = $_POST['numero'];
-    $endereco = $_POST['endereco'];
-
-
     try {
         $contato = new Contato($nome, $email, $numero, $endereco);
         $contatoDAO = new ContatoDAO();
     
         $contatoDAO->save($contato);
-        echo 'Dados Inseridos';
-        // @TODO apagar esse visualizador
-        foreach($_POST as $incoming) {
-            echo htmlentities($incoming), '</br>';
-        }
+        echo 'Inseridos';
     } catch (Exception $ex) {
         error_log($e->getMessage());
         http_response_code(500);
